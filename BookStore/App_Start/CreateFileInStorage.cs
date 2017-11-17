f33fb1778dad0ee1dac074b7639b6e0e5d9fd6af
Artuom <kazlouski.artuom@gmail.com>
2017-11-17 16:35:47 +0300

    enable swagger for api

---
+++ BookStore/App_Start/CreateFileInStorage.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using BookStore.App_Start;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.File;
+using Serilog;
+
+[assembly: PreApplicationStartMethod(typeof(CreateFileInStorage), "CreateFile")]
+
+namespace BookStore.App_Start
+{
+    public class CreateFileInStorage
+    {
+        public static void CreateFile()
+        {
+            var storageConnection = ConfigurationManager.ConnectionStrings["AzureStorageConnection"].ToString();
+            
+            var storageAccount = CloudStorageAccount.Parse(storageConnection);
+            var fileClient = storageAccount.CreateCloudFileClient();
+            var share = fileClient.GetShareReference("logappstartsshare");
+            share.CreateIfNotExists();
+
+            // Get a reference to the root directory for the share.
+            CloudFileDirectory rootDir = share.GetRootDirectoryReference();
+
+            // Get a reference to the directory we created previously.
+            CloudFileDirectory sampleDir = rootDir.GetDirectoryReference("CustomLogs");
+            sampleDir.CreateIfNotExists();
+            // Ensure that the directory exists.
+            if (sampleDir.Exists())
+            {
+                var dateString = (DateTime.Now.ToString("s")).Replace(':', '-');
+
+                // Get a reference to the file we created previously.
+                CloudFile file = sampleDir.GetFileReference($"Start-{dateString}.txt");
+                try
+                {
+                    file.Create(555);
+                    file.UploadText($"{Environment.MachineName} - {dateString}");
+                }
+                catch (Exception ex)
+                {
+                    var s = ex.Message;
+                    throw;
+                }
+            }
+        }
+    }
+}
--- BookStore/Global.asax.cs
+++ BookStore/Global.asax.cs
@@ -1,18 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
+﻿using System.Configuration;
 using System.Data.Entity;
-using System.Data.Entity.Infrastructure;
-using System.IO;
-using System.Linq;
-using System.Web;
-using System.Web.Hosting;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using BookStore.App_Start;
 using BookStore.Models;
-using Microsoft.WindowsAzure.Storage;
-using Microsoft.WindowsAzure.Storage.File;
 using Serilog;
 
 namespace BookStore
@@ -26,44 +19,15 @@ namespace BookStore
 
 			AreaRegistration.RegisterAllAreas();
 			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-			RouteConfig.RegisterRoutes(RouteTable.Routes);
+		    WebApiConfig.Register(GlobalConfiguration.Configuration);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
 			BundleConfig.RegisterBundles(BundleTable.Bundles);
-
-		    var storageConnection = ConfigurationManager.ConnectionStrings["AzureStorageConnection"].ToString();
+            
+            var storageConnection = ConfigurationManager.ConnectionStrings["AzureStorageConnection"].ToString();
 
             Log.Logger = new LoggerConfiguration()
                 .WriteTo.AzureTableStorage(storageConnection, storageTableName: "CriticalErrors")
                 .CreateLogger();
-
-		    var storageAccount = CloudStorageAccount.Parse(storageConnection);
-		    var fileClient = storageAccount.CreateCloudFileClient();
-		    var share = fileClient.GetShareReference("logappstartsshare");
-		    share.CreateIfNotExists();
-
-		    // Get a reference to the root directory for the share.
-		    CloudFileDirectory rootDir = share.GetRootDirectoryReference();
-
-		    // Get a reference to the directory we created previously.
-		    CloudFileDirectory sampleDir = rootDir.GetDirectoryReference("CustomLogs");
-		    sampleDir.CreateIfNotExists();
-		    // Ensure that the directory exists.
-		    if (sampleDir.Exists())
-		    {
-		        var dateString = (DateTime.Now.ToString("s")).Replace(':', '-');
-
-                // Get a reference to the file we created previously.
-                CloudFile file = sampleDir.GetFileReference($"Start-{dateString}.txt");
-		        try
-		        {
-                    file.Create(555);
-		            file.UploadText($"{Environment.MachineName} - {dateString}");
-		        }
-		        catch (Exception ex)
-		        {
-		            var s = ex.Message;
-		            throw;
-		        }
-		    }
         }
 	}
 }
